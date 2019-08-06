65b3ed327dafdbb0918a5cee118942ab651cd3ce
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2019-08-06 14:50:21 -0300

    this is the first commit ◆

--- AnimatedView/ViewController.swift
+++ AnimatedView/ViewController.swift
@@ -10,11 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    private var sliderValue : Int = 0
+    
+    // MARK: - Outlets
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var curveValue: UILabel!
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slider.value = Float(sliderValue)
+        slider.minimumValue = 0.00
+        slider.maximumValue = 800.00
+        curveValue.text = String(Int(sliderValue))
     }
+    
+    // MARK: - UI Actions
+    @IBAction func animationIcon(_ sender: Any) {
+        animate(view: view1, fromPoint: CGPoint(x: 40, y: 40), toPoint: CGPoint(x: 40, y: view.bounds.size.height - 140))
+    }
+    
+    @IBAction func slideToValue(_ sender: Any) {
+        sliderValue = Int(slider.value)
+        curveValue.text = String(Int(sliderValue))
+        print("Ⓜ️ slider value: \(slider.value)")
+    }
+    
+    
 
-
+    // MARK: - Animation Helper
+    private func animate(view : UIView, fromPoint start : CGPoint, toPoint end: CGPoint){
+        // The animation
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        
+        // Animation's path
+        let path = UIBezierPath()
+        
+        // Move the "cursor" to the start
+        path.move(to: start)
+        
+        // Calculate the control points
+//        let c1 = CGPoint(x: start.x + 64, y: start.y)
+//        let c2 = CGPoint(x: end.x,        y: end.y - 128)
+        let c1 = CGPoint(x: start.x + CGFloat(sliderValue), y: start.y)
+        let c2 = CGPoint(x: end.x,        y: end.y)
+        
+        // Draw a curve towards the end, using control points
+        path.addCurve(to: end, controlPoint1: c1, controlPoint2: c2)
+        
+        // Use this path as the animation's path (casted to CGPath)
+        animation.path = path.cgPath;
+        
+        // The other animations properties
+        animation.fillMode              = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        animation.duration              = 1.0
+        animation.timingFunction        = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeIn)
+        
+        // Apply it
+        view.layer.add(animation, forKey:"trash")
+    }
+    
+    
 }
 
